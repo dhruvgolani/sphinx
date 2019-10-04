27b35fd116fce36d08ecca16d179449b07a92aa2
anmolmittal26 <2017umt1501@mnit.ac.in>
2019-10-04 23:52:42 +0530

    anmolmittal26

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ class Database{
 
 	$this->host = 'localhost';
     $this->user = 'root';
-    $this->pass = '';
+    $this->pass = 'root';
     $this->db = 'sphinx';
 	/*$this->host = 'localhost';
     $this->user = 'root';
